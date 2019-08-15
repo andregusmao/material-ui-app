0196b05c9d5f1f11cb6ee539ad17f8ce8263ce45
André Gusmão <agusmao@infoagesistemas.com.br>
2019-08-15 17:11:01 -0300

    updating

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,6 +1,6 @@
-import React, { useReducer } from "react";
-import { Container, Grid, TextField, Button } from "@material-ui/core";
-import { Save as SaveIcon, Cancel as CancelIcon } from "@material-ui/icons";
+import React, { useReducer, useState } from "react";
+import { Container, Grid, TextField, Button, Typography, Snackbar, SnackbarContent } from "@material-ui/core";
+import { Check as CheckIcon } from "@material-ui/icons";
 
 import useStyles from "./styles";
 
@@ -10,42 +10,58 @@ export default function Login() {
   const [values, setValues] = useReducer(
     (state, update) => ({ ...state, ...update }),
     {
-      firstname: "",
-      lastname: ""
+      email: '',
+      password: ''
     }
   );
 
+  const [error, setError] = useState('');
+
   function handleInputs(event) {
     setValues({
-      [event.target.name]: event.target.value
+      [event.target.id]: event.target.value
     });
+    setError('');
+  }
+
+  function handleLogin(event) {
+    event.preventDefault();
+    if (values.email === '' || values.password === '') {
+      setError('Informe o e-mail e a senha');
+    } else {
+      setError('');
+    }
   }
 
   return (
-    <Container className={styles.container}>
-      <form noValidate autoComplete="off">
+    <Container maxWidth="sm" className={styles.container}>
+      <form noValidate autoComplete="off" onSubmit={handleLogin}>
         <Grid container direction="row" spacing={2}>
-          <Grid item sm={6}>
+          <Grid item sm={12} md={12} xs={12}>
             <TextField
               className={styles.textField}
-              id="firstname"
-              label="First Name"
+              id="email"
+              label="E-mail"
               margin="dense"
               variant="outlined"
+              type="email"
+              placeholder="Digite o seu e-email"
               fullWidth
-              value={values.firstname}
+              value={values.email}
               onChange={handleInputs}
             />
           </Grid>
-          <Grid item sm={6}>
+          <Grid item sm={12} md={12} xs={12}>
             <TextField
               className={styles.textField}
-              id="lastname"
-              label="Last Name"
+              id="password"
+              label="Senha"
               margin="dense"
               variant="outlined"
+              type="password"
+              placeholder="Digite a sua senha"
               fullWidth
-              value={values.lastname}
+              value={values.password}
               onChange={handleInputs}
             />
           </Grid>
@@ -57,16 +73,30 @@ export default function Login() {
           direction="column"
           justify="center"
         >
-          <Grid item sm={12}>
-            <Button className={styles.button} variant="contained" color="primary">
-              <SaveIcon className={styles.buttonIcon} /> Salvar
-            </Button>
-            <Button className={styles.button} variant="contained" color="primary">
-              <CancelIcon className={styles.buttonIcon} /> Cancelar
+          <Grid item sm={12} md={12} xs={12}>
+            <Button className={styles.button} type="submit" variant="contained" color="primary">
+              <CheckIcon className={styles.buttonIcon} /> Entrar
             </Button>
           </Grid>
         </Grid>
       </form>
+      <Snackbar
+        anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: 'center'
+        }}
+        open={error !== ''}
+        autoHideDuration={6000}
+        variant="error"
+      >
+        <SnackbarContent
+          message={
+            <span id="client-snackbar">
+              {error}
+            </span>
+          }
+        />
+      </Snackbar>
     </Container>
   );
 }
--- src/pages/Login/styles.js
+++ src/pages/Login/styles.js
@@ -2,6 +2,8 @@ import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles(theme => ({
   container: {
+    width: 480,
+    maxWidth: 360,
     marginTop: 80,
     flexGrow: 1
   },
